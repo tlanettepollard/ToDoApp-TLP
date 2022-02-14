6d557d3d043e378d1a6ca3d96c4734834cd2901d
T. Lanette <TLPollard4516@gmail.com>
2022-02-13 20:04:22 -0500

    Updates

---
+++ src/components/data.js
@@ -0,0 +1,34 @@
+const dataList = [
+    {
+        text: 'Make hair appointment',
+        id: 0,
+        completed: true,
+    }, 
+    {
+        text: 'Vacuum',
+        id: 1,
+        completed: false,
+    },
+    {
+        text: 'Pick up groceries',
+        id: 2,
+        completed: false,
+    },
+    {
+        text: 'Wash clothes',
+        id: 3,
+        completed: false,
+    },
+    {
+        text: 'Sort mail',
+        id: 4,
+        completed: false,
+    },
+    {
+        text: 'Take cats to the vet',
+        id: 5,
+        completed: false,
+    },
+]
+
+export default dataList
