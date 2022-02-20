1e98c7c72f63582dd57d07644d2bee432d26792f
T. Lanette <TLPollard4516@gmail.com>
2022-02-20 15:32:01 -0500

    Updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { ReactComponent as LightIcon } from '../images/icon-sun.svg';
+import { ReactComponent as DarkIcon } from '../images/icon-moon.svg';
 
-const Header = () => {
+
+const Header = ({ theme, toggleTheme }) => {
     return (
         <header className='header'>
-            <h1>Todo</h1>
+            <h1 className='header__title'>Todo</h1>
+            <button aria-label='Switch Theme' className='btn-switch' onClick={() => toggleTheme()}>
+                { theme === 'light' ? <DarkIcon/> : <LightIcon />}
+            </button>
         </header>
     );
 };
