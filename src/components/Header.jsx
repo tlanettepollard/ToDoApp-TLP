aa1bf572094900e37c921ca5889a576d7b528d66
T. Lanette <TLPollard4516@gmail.com>
2022-02-20 20:51:11 -0500

    Updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 
-const Header = ({ theme, toggleTheme }) => {
+const Header = () => {
     return (
         <header className='header'>
             <h1 className='header__title'>Todo</h1>
