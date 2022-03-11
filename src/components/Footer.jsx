ab11097ea7dcf2caf13df915f23219c1fdfde9f8
T. Lanette <TLPollard4516@gmail.com>
2022-03-11 18:32:25 -0500

    Style updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 export default function Footer() {
     return (
         <footer className='attribution'>
-            <p>
+            <p className='footer-text'>
                 Challenge by{''}
                 <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>
             </p>
-            <p>
+            <p className='footer-text'>
                 Coded by <a href="https://tlp-portfolio.vercel.app/">Trista Lanette Pollard</a>.
             </p>
         </footer>
